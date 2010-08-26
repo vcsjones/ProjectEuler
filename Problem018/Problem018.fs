0d2f03bf6f1671ac3190e0de53bcef550af09951
Kevin Jones <vcsjones@gmail.com>
2010-08-25 21:15:30 -0400

    Committing solution to problem 18 and 67

---
+++ Problem018/Problem018.fs
@@ -0,0 +1,17 @@
+﻿let answer = 
+   System.IO.File.ReadLines(".\\triangle.txt")
+   |> Seq.map(fun x -> x.Split(' ') |> Seq.map(fun y -> y|>int))
+   |> Seq.fold(fun acc x -> 
+            let rowLen = Seq.length x
+            x |> Seq.mapi(fun i y -> 
+                              let current = fun () -> y+(Seq.nth i acc)
+                              let previous = fun () -> y+(Seq.nth (i-1) acc)
+                              if i=0 then current()
+                              elif i=rowLen-1 then previous()
+                              else System.Math.Max(current() , previous())
+                           )
+              |> Seq.toList
+         ) [0;]
+   |> List.max
+
+printfn "answer = %d" answer
--- Problem026/Problem026.fs
+++ Problem026/Problem026.fs
@@ -1,1 +1,18 @@
-﻿let
+﻿let sieve limit =
+   let mults p = p :: [ (p*p) .. p .. limit ] |> Set.ofList
+   let prune y = Set.minElement y, Set.minElement y |> mults |> Set.difference y
+   let rec loop (p, pset) ans =
+      match pset with
+      | x when x = Set.empty -> Set.add p ans
+      | _ -> loop (prune pset) (Set.add p ans)
+   loop ([ 2L .. limit ] |> Set.ofList |> prune) Set.empty |> Set.toList
+
+
+let rec gcd n1 n2 = 
+   match n2 with
+   | 0 -> n1
+   | _ -> gcd n2 (n1%n2)
+
+let multOrder a m =
+   assert(gcd a m = 1)
+   
