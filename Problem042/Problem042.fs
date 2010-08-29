fc1a875ca4dc70d6217cc80c077894073dc7da79
Kevin Jones <vcsjones@gmail.com>
2010-08-28 21:04:25 -0400

    Finished problem 42

---
+++ Problem042/Problem042.fs
@@ -0,0 +1,21 @@
+﻿open System.IO
+
+let t n = 0.5*n*(n+1.0)
+
+let wordValue (str:string) = 
+   str.ToCharArray()
+   |> Seq.map(fun x -> int(x) - 64)
+   |> Seq.sum
+   |> float
+
+let isTriangleNumber n = 
+   [for x in [1.0..n] -> t x]
+   |> List.exists(fun x -> x = n)
+
+let answer = 
+   File.ReadLines(".\\words.txt")
+   |> Seq.filter(fun x -> isTriangleNumber (wordValue x))
+   |> Seq.toArray
+   |> Seq.length
+
+printfn "answer = %d" answer
