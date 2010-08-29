2cba69acd4266deed6a5d26adae7f6279ef95650
Kevin Jones <vcsjones@gmail.com>
2010-08-28 23:05:11 -0400

    Solved problem 34. Very inefficient. Need to find a better algorithm.

---
+++ Problem034/Problem034.fs
@@ -0,0 +1,13 @@
+﻿let factorial n = [1 .. n] |> Seq.fold(fun acc n -> acc*n) 1
+
+
+let sumFactorDigits n = 
+    n.ToString().ToCharArray()
+    |> Seq.map(fun x -> factorial(int(x) - 0x30))
+    |> Seq.sum
+
+let answer = 
+    [3..100000]
+    |> Seq.map(fun x -> (x, sumFactorDigits x))
+    |> Seq.filter(fun (x, y) -> x=y)
+    |> Seq.iter(fun (x, _) -> printfn "%d" x)
