5305e0b87138b87c2a71ddb96dd673fa3352d5e7
Kevin Jones <vcsjones@gmail.com>
2010-08-30 00:45:20 -0400

    Finished problem 56.

---
+++ Problem056/Problem056.fs
@@ -0,0 +1,12 @@
+﻿let answer = 
+   seq {
+         for a in [1I..99I] do
+            for b in [1..99] do
+               yield a**b
+       }
+
+   |> Seq.map(fun x -> x.ToString("R").ToCharArray())
+   |> Seq.map(fun x -> x |> Seq.map(fun y -> int(y) - 0x30) |> Seq.sum)
+   |> Seq.max
+
+printfn "answer = %d" answer
