7e848173720a55b134f41f231283372db0d6e7fb
Kevin Jones <vcsjones@gmail.com>
2010-09-06 19:39:40 -0400

    Implemented problem 45.

---
+++ Problem045/Problem045.fs
@@ -0,0 +1,14 @@
+﻿//Skip all triangle numbers. All Triangle numbers are also Hexagonal numbers.
+
+let pentagonal n = n * (3L*n-1L)/2L
+let hexagonal n = n * (2L*n-1L)
+
+let pentagonals = set [for n in [166L..100000L] -> pentagonal n]
+let hexagonals = set [for n in [143L..100000L] -> hexagonal n]
+
+let answer = 
+   pentagonals
+   |> Set.intersect hexagonals
+   |> Set.minElement
+
+printfn "answer = %d" answer
