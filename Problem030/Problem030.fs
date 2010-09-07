e57914ca418e858a26b7670697c5c67e56ce5bd3
Kevin Jones <vcsjones@gmail.com>
2010-09-06 21:05:56 -0400

    Committing solution to problem 30

---
+++ Problem030/Problem030.fs
@@ -0,0 +1,11 @@
+﻿let fifthSum (n:float) = 
+   Seq.map(fun x -> float(int(x)-0x30)**5.0) (string(n))
+   |> Seq.fold(fun acc x -> acc+x) 0.0
+
+let answer = 
+   [2.0 .. (9.0**5.0)*6.0]
+   |> Seq.map(fun x -> (x, fifthSum x))
+   |> Seq.filter(fun (x, y) -> x=y)
+   |> Seq.sumBy fst
+
+printfn "answer = %f" answer
