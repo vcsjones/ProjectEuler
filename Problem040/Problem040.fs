b627f0eb4299952e01983b082dfad8a315e8d320
Kevin Jones <vcsjones@gmail.com>
2010-09-09 03:56:20 -0400

    Completed problem 40

---
+++ Problem040/Problem040.fs
@@ -0,0 +1,11 @@
+﻿let sb = new System.Text.StringBuilder()
+
+let superString = 
+   [1..1000000] |> Seq.fold(fun acc x -> sb.Append(x)) sb
+   |> string
+
+let d n = int(superString.[n-1]) - 0x30
+
+let answer = d(1) * d(10) * d(100) * d(1000) * d(10000) * d(100000)
+
+printfn "answer = %d" answer
