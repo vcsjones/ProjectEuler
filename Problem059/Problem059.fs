3bddf6e7554b0475ebbdc59a7cda3f68f1d9bbf6
Kevin Jones <vcsjones@gmail.com>
2010-09-09 05:23:02 -0400

    Committing solution to problem 59

---
+++ Problem059/Problem059.fs
@@ -0,0 +1,33 @@
+﻿let cipherData = 
+   System.IO.File.ReadAllText(".\\cipher1.txt").Split(',')
+   |> Array.map(fun x -> x.Trim() |> byte)
+
+let passwords = seq {
+   for x in ['a'..'z'] do
+      for y in ['a'..'z'] do
+         for z in ['a'..'z'] do
+            yield [|byte(x);byte(y);byte(z)|]
+   }
+
+
+let specMod n = 
+   match n % 3 with
+   | 1 -> 0
+   | 2 -> 1
+   | 0 -> 2
+   | _ -> failwith("WTF")
+
+let answer =
+   seq {
+      for pwd in passwords do
+         let decryptedBytes = 
+            cipherData
+            |> Array.mapi(fun i x -> pwd.[specMod (i+1)] ^^^ x)
+         let text = System.Text.Encoding.ASCII.GetString(decryptedBytes)
+         if text.Contains("the") && text.Contains("was") && text.Contains("so") && text.EndsWith(".") then
+            yield decryptedBytes |> Array.fold(fun acc x -> acc + int(x)) 0
+   }
+   |> Seq.head
+
+
+printfn "answer = %d" answer
