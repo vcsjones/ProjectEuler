5d60eeca3cf2ed2d559ccc5d087635b1fe10481a
Kevin Jones <vcsjones@gmail.com>
2010-09-10 00:27:40 -0400

    Committing solution to problem 27

---
+++ Problem027/Problem027.fs
@@ -0,0 +1,19 @@
+﻿let quad n a b = n*n + a*n + b
+
+let isPrime n = n > 0 && [|2.. (n|>float|>sqrt|>int)|] |> Array.exists(fun y -> n <> y && n % y=0) |> not
+
+let answer = 
+   seq {
+   for a in {-999..999} do
+      for b in {1..999} do
+         let primes = 
+            Seq.unfold(fun x -> Some(x, x+1)) 0
+            |> Seq.takeWhile(fun x -> isPrime (quad x a b))
+         let len = primes |> Seq.length
+         yield (len, a, b)
+   }
+   |> Seq.maxBy(fun (x, _, _) -> x)
+
+let _, a, b = answer
+
+printfn "a = %d; b = %d" a b
