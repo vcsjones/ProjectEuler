f2d2ebb1ff930f330d380671fb79391351bb22a5
Kevin Jones <vcsjones@gmail.com>
2010-09-10 00:28:34 -0400

    Committing progress on proglem 145

---
+++ Problem145/problem145.fs
@@ -0,0 +1,19 @@
+﻿let isReversible n = 
+   let str = n.ToString()
+//   let digits = str.ToCharArray() |> Array.map(fun x -> int(x) - 0x30)
+//   if digits |> Array.forall(fun x -> x%2=0) then
+//      false
+//   else
+   let revInt = int(new string(str.ToCharArray() |> Array.rev))
+   (n + revInt).ToString().ToCharArray()
+   |> Array.exists(fun x -> (int(x) - 0x30)%2=0)
+   |> not
+      
+
+
+let answer = 
+   {1..999}
+   |> Seq.filter (fun x -> isReversible x)
+   |> Seq.iter(fun x -> printfn "%d" x)
+
+//printfn "answer = %d" answer
