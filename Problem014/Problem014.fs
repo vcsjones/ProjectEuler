b60aed5794c521939e20466e0bad25efede08117
Kevin Jones <vcsjones@gmail.com>
2010-09-10 00:28:11 -0400

    Minor performance enhancement to problem 14.

--- Problem014/Problem014.fs
+++ Problem014/Problem014.fs
@@ -1,8 +1,10 @@
-﻿let rec collatz n =
-   match n with
-   | n when n = 1L -> 1
-   | n when n%2L = 0L -> collatz(n/2L) + 1
-   | _ -> collatz(3L*n+1L) + 1
+﻿let collatz n =
+   let rec loop(n,x) = 
+      match n with
+      | n when n = 1L -> x
+      | n when n%2L = 0L -> loop(n/2L,x+1)
+      | _ -> loop(3L*n+1L,x+1)
+   loop(n,1)
 
 let answer =
     Async.Parallel [ for i in 13L..999999L -> async { return (i, collatz i) } ]
