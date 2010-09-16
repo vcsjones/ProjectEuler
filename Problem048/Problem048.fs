db0677cb3df5f5cbb70bc3097f9636e0d4096dab
Kevin Jones <vcsjones@gmail.com>
2010-09-15 21:33:42 -0400

    Cleaning up problem 48

--- Problem048/Problem048.fs
+++ Problem048/Problem048.fs
@@ -1,9 +1,7 @@
-﻿let watcher = System.Diagnostics.Stopwatch.StartNew()
-let hugeSum =
+﻿let hugeSum =
    ([1I..1000I]
    |> Seq.fold(fun acc x -> acc + (x**int(x))) (0I)
    ).ToString("R")
 
 let answer = hugeSum.[(hugeSum.Length - 10)..]
-watcher.Stop()
 printfn "answer = %s" answer
