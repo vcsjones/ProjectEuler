efb4736c389f3c2f6904450660aeea4449f0f2cd
Kevin Jones <vcsjones@gmail.com>
2010-09-21 01:07:56 -0400

    Cleaning up problem 19

--- Problem019/Problem019.fs
+++ Problem019/Problem019.fs
@@ -4,7 +4,6 @@ let answer =
    Seq.unfold(fun (x:DateTime) -> Some(x, x.AddDays(1.0))) (DateTime(1901, 1, 1))
    |> Seq.filter(fun x -> x.DayOfWeek = DayOfWeek.Sunday && x.Day = 1)
    |> Seq.takeWhile(fun x -> x <= DateTime(2000, 12, 31))
-   |> Seq.toArray
-   |> Array.length
+   |> Seq.length
 
 printfn "answer = %d" answer
