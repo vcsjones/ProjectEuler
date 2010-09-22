9c672c4ddc96197f0d6c7842246eafa248ace7bf
Kevin Jones <vcsjones@gmail.com>
2010-09-21 20:26:21 -0400

    Simplifying problem 8

--- Problem008/Problem008.fs
+++ Problem008/Problem008.fs
@@ -1,6 +1,4 @@
-﻿open System.Text.RegularExpressions
-
-let string =  Regex.Replace(
+﻿let string =  System.Text.RegularExpressions.Regex.Replace(
                             "73167176531330624919225119674426574742355349194934
                              96983520312774506326239578318016984801869478851843
                              85861560789112949495459501737958331952853208805511
