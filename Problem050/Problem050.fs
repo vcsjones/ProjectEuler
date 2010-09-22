02cd891973ec3743276c83979d2b6522ba467fa9
Kevin Jones <vcsjones@gmail.com>
2010-09-22 01:16:11 -0400

    Work in progress for problem 050.

---
+++ Problem050/Problem050.fs
@@ -0,0 +1,32 @@
+﻿open System 
+open System.Collections.Generic 
+
+let sieve n = 
+    seq { 
+        yield 2 
+        let knownComposites = new HashSet<int>() 
+        for i in 3 .. 2 .. n do 
+            let found = knownComposites.Contains(i) 
+            if not found then 
+                yield i 
+            do for j in i .. i .. n do 
+                   knownComposites.Add(j) |> ignore 
+    }
+
+let primes = sieve 1000000
+
+let check n = 
+   async {
+      return Some(n, 0)
+   }
+
+let answer = 
+   primes
+   |> Seq.map check
+   |> Async.Parallel
+   |> Async.RunSynchronously
+   |> Seq.choose(fun x -> x)
+   |> Seq.maxBy snd
+   |> fst
+
+printfn "answer = %d" answer
