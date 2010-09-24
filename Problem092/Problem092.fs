7c21a1d599d6def08c21f6425f280c3a25f8ffc1
Kevin Jones <vcsjones@gmail.com>
2010-09-24 00:27:14 -0400

    Committing solution to problem 92.

---
+++ Problem092/Problem092.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.Threading
+open System.Threading.Tasks
+
+let sumDigitSquares n =
+      n.ToString().ToCharArray()
+      |> Array.map(fun x -> (float x - 48.0)**2.0)
+      |> Array.sum
+      |> int
+
+let findDigitSquare n = 
+   let rec loop l = 
+      match l with
+      | 1 -> false
+      | 89 -> true
+      | x -> loop(sumDigitSquares(x))
+   loop n
+
+let actImpl n pls loc = 
+   if findDigitSquare n then loc + 1
+   else loc
+
+let mutable counter = 0
+
+Parallel.For(1, 10000000, (fun () -> 0), actImpl, (fun n -> Interlocked.Add(&counter, n) |> ignore)) |> ignore
+
+printfn "answer = %d" counter
