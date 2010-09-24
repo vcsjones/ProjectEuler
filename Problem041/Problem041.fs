4d2ca6e5631c19567583849b08b1c16de477ae30
Kevin Jones <vcsjones@gmail.com>
2010-09-23 23:33:37 -0400

    Committing solution to problem 41

--- Problem041/Problem041.fs
+++ Problem041/Problem041.fs
@@ -1,1 +1,27 @@
-﻿
+﻿open System.Collections.Generic 
+
+let isPandigital n = 
+   let str = new string(n.ToString().ToCharArray() |> Array.sort)
+   let matcher = [1..str.Length] |> Seq.map string |> Seq.reduce(fun acc x -> acc+x)
+   str = matcher
+
+let sieve n = 
+    seq { 
+        yield 2 
+        let knownComposites = new HashSet<int>() 
+        for i in 3 .. 2 .. n do 
+            let found = knownComposites.Contains(i) 
+            if not found then 
+                yield i 
+            do for j in i .. i .. n do 
+                   knownComposites.Add(j) |> ignore
+   }
+
+let primes = sieve 10000000 |> Seq.cache
+
+let answer = 
+   primes
+   |> Seq.filter isPandigital
+   |> Seq.max
+
+printfn "answer = %d" answer
