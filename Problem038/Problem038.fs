66fbc8fffd448953102e065754b48a002376b960
Kevin Jones <vcsjones@gmail.com>
2010-09-25 19:32:51 -0400

    Completed problem 38

---
+++ Problem038/Problem038.fs
@@ -0,0 +1,21 @@
+﻿let isPandigital n = 
+   let str = new string(n.ToString().ToCharArray() |> Array.sort)
+   "123456789" = str
+
+let countFactors (n:int) = 
+      let rec loop (s:string, f:int) =
+         let current = s + string(f*n)
+         match current.Length with
+         | l when l >= 9 -> if isPandigital current then Some(int current) else None
+         | _ -> loop(current, f+1)
+      loop("", 1)
+
+let answer = 
+   [2..500000]
+   |> List.map(fun x -> async {return countFactors x })
+   |> Async.Parallel
+   |> Async.RunSynchronously
+   |> Seq.choose(fun x -> x)
+   |> Seq.max
+
+printfn "answer = %d" answer
