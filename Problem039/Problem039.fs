552b45292f7aa2ebb2ae0b02cb5b39285652badf
vcsjones <vcsjones@gmail.com>
2011-07-01 13:23:20 -0400

    Solved problem 39

---
+++ Problem039/Problem039.fs
@@ -0,0 +1,29 @@
+﻿[<StructuralComparison; StructuralEquality>]
+type Triangle =
+    {
+        a : float
+        b : float
+        c : float
+        p : float
+    }
+
+let maximum = 1000.0
+
+let rightTriangles = 
+    seq {
+            for a in [1.0 .. maximum] do
+                for b in [1.0 .. maximum] do
+                    let c = (a*a + b*b) ** 0.5
+                    let p = a + b + c
+                    if p <= maximum then
+                        if a * a + b * b = c*c then
+                            yield {a = a; b = b; c = c; p = p}
+                        }
+printfn "%f"
+    (
+        rightTriangles
+        |> Seq.distinct
+        |> Seq.groupBy(fun x -> x.p)
+        |> Seq.maxBy(fun (x, y) -> y |> Seq.length)
+        |> fst
+    )
