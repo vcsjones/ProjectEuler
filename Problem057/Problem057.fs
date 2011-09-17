dc5d8271a7950128e849512b850c2c730e20cc6c
Kevin Jones <vcsjones@gmail.com>
2011-09-17 10:13:28 -0400

    Committing solution to Problem 57

---
+++ Problem057/Problem057.fs
@@ -0,0 +1,10 @@
+﻿let generate =
+    Seq.unfold (fun (n, d) -> Some((n, d), (d*2I+n, d+n))) (3I, 2I)
+
+let answer = 
+    generate
+    |> Seq.take 1000
+    |> Seq.filter(fun (n, d) -> n.ToString("R").Length > d.ToString("R").Length)
+    |> Seq.length
+
+printfn "%A" answer
