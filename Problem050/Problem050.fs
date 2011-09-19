aa9b34f1d561937baad4607ee04fdf1d4c4efd81
Kevin Jones <vcsjones@gmail.com>
2011-09-18 21:52:04 -0400

    Solved problem 58. Performance review candidate

--- Problem050/Problem050.fs
+++ Problem050/Problem050.fs
@@ -12,3 +12,24 @@ let sieve n =
             do for j in i .. i .. n do 
                    knownComposites.Add(j) |> ignore 
     }
+
+let primes = sieve 1000000 |> Seq.toArray
+
+let primeCounter n = 
+    seq {
+        for i in 1..50 do 
+            let count =
+                primes
+                |> Seq.windowed i
+                |> Seq.filter(fun x -> x |> Array.sum = n)
+                |> Seq.map(fun x -> x |> Array.length)
+            if count |> Seq.isEmpty then yield 0
+            else yield count |> Seq.max
+    }
+    |> Seq.max
+
+let answer = 
+    primes
+    |> Array.maxBy(primeCounter)
+
+printfn "%i" answer
---
+++ Problem058/Problem058.fs
@@ -0,0 +1,21 @@
+﻿let isPrime n =
+    if n = 1L then false
+    else [|2L.. (n|>float|>sqrt|>int64)|]
+         |> Array.exists(fun y -> n <> y && n % y=0L)
+         |> not
+
+let diagonals = 
+    Seq.unfold(fun (n, c, s, w, t, p) ->
+                                let prime = if isPrime(n+s) then p+1.0f else p
+
+                                if c = 4L then Some((n, c, s, w, t, p), (n+s, 1L, s+2L, w, t+1.0f, prime))
+                                else Some((n, c, s, w, t, p), (n+s, c+1L, s, (if c = 1L then w+2L else w), t+1.0f, prime))
+                ) (1L, 1L, 2L, 1L, 1.0f, 0.0f)
+let answer = 
+    diagonals
+    |> Seq.map(fun (n, c, s, w, t, p) -> (n, c, s, w, t, p, p/t))
+    |> Seq.skipWhile(fun (n, c, s, w, t, p, a) -> n = 1L || a > 0.1f)
+    |> Seq.map(fun (_, _, _, w, _, _, _) -> w)
+    |> Seq.head
+
+printfn "%i" answer
