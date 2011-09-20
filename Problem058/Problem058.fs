4a53c7e58c05a2d4d0ad441176d7be03e902da51
Kevin Jones <vcsjones@gmail.com>
2011-09-20 09:53:52 -0400

    Cleaning up problem 58 to be more readable.

--- Problem058/Problem058.fs
+++ Problem058/Problem058.fs
@@ -4,18 +4,18 @@
          |> Array.exists(fun y -> n <> y && n % y=0L)
          |> not
 
-let diagonals = 
-    Seq.unfold(fun (n, c, s, w, t, p) ->
-                                let prime = if isPrime(n+s) then p+1.0f else p
 
-                                if c = 4L then Some((n, c, s, w, t, p), (n+s, 1L, s+2L, w, t+1.0f, prime))
-                                else Some((n, c, s, w, t, p), (n+s, c+1L, s, (if c = 1L then w+2L else w), t+1.0f, prime))
-                ) (1L, 1L, 2L, 1L, 1.0f, 0.0f)
 let answer = 
-    diagonals
-    |> Seq.map(fun (n, c, s, w, t, p) -> (n, c, s, w, t, p, p/t))
-    |> Seq.skipWhile(fun (n, c, s, w, t, p, a) -> n = 1L || a > 0.1f)
-    |> Seq.map(fun (_, _, _, w, _, _, _) -> w)
-    |> Seq.head
+    let rec wrap (n, c, s, w, t, p) =
+        match (p/t) with
+        | a when a <= 0.1f && n > 1L -> w
+        | _ ->
+                let prime = if isPrime(n+s) then p+1.0f else p                 
+                match c with
+                | 4L -> wrap(n+s, 1L, s+2L, w, t+1.0f, prime)
+                | 1L -> wrap(n+s, c+1L, s, w+2L, t+1.0f, prime)
+                | _  -> wrap(n+s, c+1L, s, w, t+1.0f, prime)
+    wrap(1L, 1L, 2L, 1L, 1.0f, 0.0f)
 
-printfn "%i" answer
+
+printfn "answer = %i" answer
