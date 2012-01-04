9fd577a727726d3cb74bce8fe7c707eb386e20ea
Kevin Jones <vcsjones@gmail.com>
2012-01-03 21:45:38 -0500

    Workable progress on problem 26 using classic long division.

--- Problem026/Problem026.fs
+++ Problem026/Problem026.fs
@@ -1,26 +1,41 @@
-﻿let sieve limit =
-   let mults p = p :: [ (p*p) .. p .. limit ] |> Set.ofList
-   let prune y = Set.minElement y, Set.minElement y |> mults |> Set.difference y
-   let rec loop (p, pset) ans =
-      match pset with
-      | x when x = Set.empty -> Set.add p ans
-      | _ -> loop (prune pset) (Set.add p ans)
-   loop ([ 2L .. limit ] |> Set.ofList |> prune) Set.empty |> Set.toList
+﻿open System.Text
+open System
 
+let divide n = 
+    let inline toInt (x:char) = int x - 0x30
+    let rec solve (sb:StringBuilder, l:int, iter:int) = 
+        if iter = 4000 then sb
+        else 
+            let result = l / n
+            sb.Append(box result) |> ignore
+            let multiplyBack = result * n
+            let difference = l - multiplyBack
+            assert (difference >= 0)
+            let diffMul = difference * 10
+            solve(sb, diffMul, iter+1)
+    let answer = solve (new StringBuilder(), 1, 1)
+    answer.ToString(1, answer.Length - 1).ToCharArray() |> List.ofArray
 
-let rec gcd n1 n2 = 
-   match n2 with
-   | 0 -> n1
-   | _ -> gcd n2 (n1%n2)
+let compare (a:seq<_>) (b:seq<_>) =
+    let equal = Seq.forall2(fun x y -> x = y)
+    equal a b
 
-let totient n = 
-   [1..n]
-   |> List.filter(fun x -> gcd x n = 1)
-   |> List.length
+let getRepGroup (chrs:List<char>) = 
+    let rec windowGroup (group:List<char>) (running:List<List<char>>) = 
+        match group with
+        | list when list |> List.length = 1 -> windowGroup list.Tail running //One character in length. Can't repeat, so skip.
+        | list -> 
+            List.Empty
+        | [] -> running |> List.maxBy List.length
+    windowGroup chrs List.Empty
 
-let multOrder a m =
-   if (gcd a m) <> 1 then failwith "Arguments are not coprime"
-   else
-      0
+(* let answer = 
+    [1..999]
+    |> Seq.map(fun x -> (x, x |> divide |> countMaxRep))
+    |> Seq.maxBy snd
+    |> fst *)
 
-printfn "test = %d" (totient 2)
+//printfn "answer = %i" answer
+
+let six = divide 6
+printfn "%s" (new string(six |> Seq.toArray))
