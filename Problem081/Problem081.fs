88304bfb17781597c5cdab08d9d272bd86dca109
unknown <admin@WIN-QU5JA8EOETQ.(none)>
2012-01-05 16:17:30 -0500

    Solved problems 81 and 99

---
+++ Problem081/Problem081.fs
@@ -0,0 +1,26 @@
+﻿open System.IO
+
+let matrix = array2D (File.ReadAllLines("matrix.txt") 
+                      |> Array.map (fun l -> l.Split(',') |> Array.map int32))
+
+let size = matrix |> Array2D.length1
+
+let sum = 
+    Array2D.init size size (fun i j -> if i = 0 && j = 0 then matrix.[i, j] else 0)
+
+let top i j = sum.[i - 1, j] + matrix.[i, j]
+let left i j = sum.[i, j - 1] + matrix.[i, j]
+
+
+
+for i = 0 to size - 1 do
+    for j = 0 to size - 1 do
+        match (i, j) with
+        | (0, 0) -> ()
+        | (_, 0) -> sum.[i, j] <- top i j
+        | (0, _) -> sum.[i, j] <- left i j
+        | (_, _) -> sum.[i, j] <- min (top i j) (left i j)
+
+let answer = sum.[size - 1, size - 1]
+
+printfn "%i" answer
---
+++ Problem099/Problem099.fs
@@ -0,0 +1,16 @@
+﻿open System.IO
+
+
+let parseLine (str:string[]) = 
+    match str with
+    | [|a; b|] ->  (a |> float, b |> float)
+    | _ -> failwith("Unexpected line")
+
+let answer = 
+    File.ReadLines("base_exp.txt")
+    |> Seq.mapi(fun i x -> (i, x.Split(',') |> parseLine))
+    |> Seq.map(fun (i, (n, e)) -> (i, e *  log n))
+    |> Seq.maxBy snd
+    |> fst
+
+printfn "answer = %i" answer
