a500435c719d7f3898e5bdb5c2bb2cb28b3881e4
Kevin Jones <kevin@vcsjones.com>
2012-01-05 16:45:36 -0500

    Solved problem 32.

--- Problem032/Problem032.fs
+++ Problem032/Problem032.fs
@@ -4,12 +4,13 @@
 
 let answer =
     seq {
-        for x in 1..50000 do
-            for y in 1..(123456789 / x) do
-                let product = x * y
-                let str = x.ToString() + y.ToString() + product.ToString()
-                if str.Length = 9 && isPandigital(str) then yield product
+        for x in [2L..100L] do
+            let s =  if x > 9L then 123L else 1234L
+            for y in [s..(10000L/(x+1L))] do
+                let str = System.String.Concat(x, y, x*y) |> int64
+                if isPandigital str then yield x*y
     }
+    |> Seq.distinct
     |> Seq.sum
 
 printfn "%i" answer
