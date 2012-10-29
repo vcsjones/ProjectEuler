020191b97a246b13c386366ce8b6556478f897db
Kevin Jones <kevin@vcsjones.com>
2012-10-29 00:00:52 -0400

    Reworking problem 54. Fairly sure that the hand type detection is correct.

--- Problem054/Problem054.fs
+++ Problem054/Problem054.fs
@@ -10,6 +10,7 @@ type Card =
       Value : int;
       Suit : Suit;
    }
+   //Assume that all suits are considered equal
    interface System.IComparable with
       member this.CompareTo(other:obj) =
          let card = other :?> Card
@@ -19,97 +20,8 @@ type Card =
       let card = obj :?> Card
       card.Value = this.Value && card.Suit = this.Suit
 
-let highCard (card:List<Card>) = Some((card |> Seq.max).Value)
-
-let xOfAKind (card:List<Card>, x:int) =
-   let grouping = card |> Seq.groupBy(fun x -> x.Value) |> Seq.filter(fun (_, x) -> x |> Seq.length > 1)
-   if grouping |> Seq.length = 1 && grouping |> Seq.head |> snd |> Seq.length = x then
-      Some(grouping |> Seq.head |> fst)
-   else
-      None
-
-let twoPair(card:List<Card>) = 
-   let grouping = card |> Seq.groupBy(fun x -> x.Value)
-   let pairs = grouping |> Seq.filter(fun (_, x) -> x |> Seq.length = 2)
-   if pairs |> Seq.length = 2 then
-      Some(pairs |> Seq.maxBy(fst) |> fst)
-   else
-      None
-
-let onePair (card:List<Card>) =
-   xOfAKind(card, 2)
-
-let threeOfAKind (card:List<Card>) =
-   xOfAKind(card, 3)
-
-let fourOfAKind (card:List<Card>) =
-   xOfAKind(card, 4)
-
-let flush card = card |> Seq.pairwise |> Seq.forall(fun (x, y) -> x.Suit = y.Suit)
-let straight card = card |> List.sort |> Seq.pairwise |> Seq.forall(fun (x, y) -> x.Value + 1 = y.Value)
-
-let isFullHouse (card:List<Card>) =
-   let fullHouse = card |> Seq.groupBy(fun x -> x.Value) |> Seq.filter(fun (_, x) -> x |> Seq.length > 1) |> Seq.length = 2
-   if fullHouse then Some((card |> Seq.max).Value) else None
-
-let isStraight (card:List<Card>) = 
-   if straight(card) then Some((card |> Seq.max).Value) else None
-
-let isFlush (card:List<Card>) =
-   if flush(card) then Some((card |> Seq.max).Value) else None
-
-let isStraightFlush (card:List<Card>) =
-   if flush(card) && straight(card) then Some((card |> Seq.max).Value) else None
-
-let isRoyalFlush (card:List<Card>) =
-   if flush(card) && straight(card) && (card |> Seq.sort |> Seq.head).Value = 10 then Some(14) else None
-
-let cardCompare (a : 'T option when 'T : equality, b : 'T option when 'T : equality) =
-   if Option.isNone(a) && Option.isNone(b) then None
-   elif Option.isNone(a) && not(Option.isNone(b)) then Some(-1)
-   elif not(Option.isNone(a)) && Option.isNone(b) then Some(1)
-   elif not(Option.isNone(a)) && not(Option.isNone(b)) then 
-      if (Option.get(a) > Option.get(b)) then Some(1)
-      elif (Option.get(a) < Option.get(b)) then Some(-1)
-      else Some(0)
-   else Some(0)
-
-let rec check (a : List<Card>, b : List<Card>, func) =
-    match cardCompare(func(a), func(b)) with
-    | Some(0) -> check(a, b, highCard)
-    | v -> v
-
-let compareHands (hand1:List<Card>, hand2:List<Card>) =
-   seq {
-      yield check(hand1, hand2, isRoyalFlush)
-      yield check(hand1, hand2, isStraightFlush)
-      yield check(hand1, hand2, fourOfAKind)
-      yield check(hand1, hand2, isFullHouse)
-      yield check(hand1, hand2, isFlush)
-      yield check(hand1, hand2, isStraight)
-      yield check(hand1, hand2, threeOfAKind)
-      yield check(hand1, hand2, twoPair)
-      yield check(hand1, hand2, onePair)
-      yield cardCompare(highCard(hand1), highCard(hand2))
-   } |> Seq.choose(fun x -> x) |> Seq.head
-
-[<StructuralEquality; CustomComparison>]
-type Hand = 
-   {
-      Cards : List<Card>;
-   }
-   interface System.IComparable with
-      member this.CompareTo(other:obj) =
-         let hand2 = other :?> Hand
-         compareHands(this.Cards, hand2.Cards)
-
-
-type Set =
-   {
-      Number : int;
-      Player1 : Hand;
-      Player2 : Hand;
-   }
+   override this.GetHashCode() = 
+      (this.Value * 251) + this.Suit.GetHashCode()
 
 let parseCard (str:string) =
    {
@@ -130,17 +42,106 @@ let parseCard (str:string) =
          | suit -> invalidArg "suit" (sprintf "Value passed in was %c." suit)
    };
 
+type Hand(list : List<Card>) = 
+   let cards = list |> List.sortBy(fun x -> x.Value) |> List.rev
+   member this.Cards
+      with get() : List<Card> = cards
 
+   member this.HighCard with get() : Card = cards |> List.head
+
+   member this.HasHighest (other : Hand) : bool =
+      let head = this.Cards
+                  |> List.zip other.Cards
+                  |> List.toSeq
+                  |> Seq.skipWhile(fun (t, o) -> t.Value = o.Value)
+                  |> Seq.head
+      (head |> snd) > (head |> fst)
+
+   member this.OnePair : int option =
+      let grouping =
+         this.Cards
+         |> List.toSeq
+         |> Seq.groupBy(fun x -> x.Value)
+         |> Seq.filter(fun (_, v) -> v |> Seq.length = 2)
+      if grouping |> Seq.length = 1 then Some(grouping |> Seq.head |> fst) else None
+
+   member this.TwoPair : int option =
+      let grouping = 
+         this.Cards
+         |> List.toSeq
+         |> Seq.groupBy(fun x -> x.Value)
+         |> Seq.filter(fun (_, v) -> v |> Seq.length = 2)
+      if grouping |> Seq.length = 2 then Some(grouping |> Seq.maxBy(fun (x, y) -> x) |> fst) else None
+
+   member this.ThreeOfAKind : int option =
+      let grouping =
+         this.Cards
+         |> List.toSeq
+         |> Seq.groupBy(fun x -> x.Value)
+         |> Seq.filter(fun (_, v) -> v |> Seq.length = 3)
+      if grouping |> Seq.length = 1 then Some(grouping |> Seq.head |> fst) else None
+
+   member this.Straight : int option =
+      let rec check list next original = 
+         match list with
+         | head :: tail when head.Value = next -> check tail (head.Value-1) original
+         | [] -> Some(original)
+         | _ -> None
+      check this.Cards this.Cards.Head.Value this.Cards.Head.Value
+      
+   member this.Flush : int option =
+      if this.Cards |> List.forall(fun x -> x.Suit = this.Cards.Head.Suit) then Some(this.Cards.Head.Value) else None
+
+   member this.FullHouse : int option =
+      let grouping = 
+         this.Cards
+         |> List.toSeq
+         |> Seq.groupBy(fun x -> x.Value)
+      let two = grouping |> Seq.filter(fun (_, v) -> v |> Seq.length = 2)
+      let three = grouping |> Seq.filter(fun (_, v) -> v |> Seq.length = 3)
+      if two |> Seq.length = 1 && three |> Seq.length = 1 then Some(three |> Seq.head |> fst) else None
+
+   member this.FourOfAKind : int option =
+      let grouping =
+         this.Cards
+         |> List.toSeq
+         |> Seq.groupBy(fun x -> x.Value)
+         |> Seq.filter(fun (_, v) -> v |> Seq.length = 4)
+      if grouping |> Seq.length = 1 then Some(grouping |> Seq.head |> fst) else None
+
+   member this.StraightFlush : int option =
+      let s = [this.Straight; this.Flush];
+      if s |> List.forall Option.isSome then s |> List.maxBy(fun x -> Option.get x) else None
+
+   member this.RoyalFlush : int option =
+      if this.StraightFlush |> Option.exists(fun x -> x = 14) then Some(14) else None
+   interface System.IComparable with
+      member this.CompareTo(otherHand:obj) =
+         let compararo (left:int option) (right:int option) : (int option) = 
+            
+         let other = otherHand :?> Hand
+         
+
+
+type Set =
+   {
+      Number : int;
+      Player1 : Hand;
+      Player2 : Hand;
+   }
+
+let testHand = new Hand([{Suit = Spade; Value = 10}; {Suit = Spade; Value = 11};
+                           {Suit = Spade; Value = 12}; {Suit = Spade; Value = 13}; {Suit = Spade; Value = 14}])
+
+printfn "%A" (testHand.Score)
+(*
 let hands = 
    System.IO.File.ReadLines(".\poker.txt")
    |> Seq.map(fun x -> x.Split(' '))
    |> Seq.mapi(fun i x -> {
                            Number = i + 1
-                           Player1 = {Cards = x |> Seq.take 5 |> Seq.map(fun y -> parseCard(y)) |> Seq.toList};
-                           Player2 = {Cards = x |> Seq.skip 5 |> Seq.take 5 |> Seq.map(fun y -> parseCard(y)) |> Seq.toList};
+                           Player1 = new Hand(x |> Seq.take 5 |> Seq.map(fun y -> parseCard(y)) |> Seq.toList);
+                           Player2 = new Hand(x |> Seq.skip 5 |> Seq.take 5 |> Seq.map(fun y -> parseCard(y)) |> Seq.toList);
                        })
    |> Seq.filter (fun x -> x.Player1 > x.Player2)
-   |> Seq.length
-   (*|> Seq.iter(fun x -> printfn "%s" (if x.Player1 > x.Player2 then "Player 1 wins set " + string(x.Number) else "Player 2 wins set " + string(x.Number)))*)
-
-printfn "%i" hands
+*)
