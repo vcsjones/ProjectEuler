37e3e00fb084c77d9867748abcf6f7d88b2fc50f
Kevin Jones <kevin@vcsjones.com>
2013-03-18 13:38:07 -0400

    Removing unused function.

--- Problem062/Problem062.fs
+++ Problem062/Problem062.fs
@@ -1,13 +1,6 @@
 ﻿let inline (!!!) x =
    (x |> uint64) * (x |> uint64) * (x |> uint64)
 
-let rec permutations list taken = 
-    seq { if Set.count taken = Array.length list then yield [] else
-          for l in list do
-            if not (Set.contains l taken) then 
-              for perm in permutations list (Set.add l taken)  do
-                yield l::perm } |> Seq.toList
-
 let max = floor(System.Math.Pow((System.UInt32.MaxValue |> uint64)*1000UL |> float, 1.0/3.0)) |> int
 let skip = floor(System.Math.Pow((System.UInt32.MaxValue |> uint64) |> float, 1.0/3.0)) |> int
 let cubes = List.init max (fun n -> (n, !!!n)) |> Seq.skip skip |> Seq.toList
