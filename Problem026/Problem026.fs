6cdb26a884d9efaf12a4700861e06b8cface4ed6
Kevin Jones <kevin@vcsjones.com>
2013-03-24 18:07:51 -0400

    Progress. 26 and 33 are now solved.

--- Problem026/Problem026.fs
+++ Problem026/Problem026.fs
@@ -1,41 +1,27 @@
-﻿open System.Text
-open System
+﻿let rec quotient n d =
+    seq {
+        yield (n, d)
+        yield! quotient ((n%d)*10) d
+    }
 
-let divide n = 
-    let inline toInt (x:char) = int x - 0x30
-    let rec solve (sb:StringBuilder, l:int, iter:int) = 
-        if iter = 4000 then sb
-        else 
-            let result = l / n
-            sb.Append(box result) |> ignore
-            let multiplyBack = result * n
-            let difference = l - multiplyBack
-            assert (difference >= 0)
-            let diffMul = difference * 10
-            solve(sb, diffMul, iter+1)
-    let answer = solve (new StringBuilder(), 1, 1)
-    answer.ToString(1, answer.Length - 1).ToCharArray() |> List.ofArray
+let repeater n d =
+    let rec counter (items:(int * int) list) (push:List<_>) = 
+        match items with
+        | head :: tail when push |> List.exists(fun x -> x = head) ->  [head] |> List.append push
+        | head :: tail -> counter tail ([head] |> List.append push)
+        | [] -> failwith "Non-repeating result encountered."
+    counter ((quotient n d) |> Seq.take 2000 |> Seq.toList) (List.empty)
 
-let compare (a:seq<_>) (b:seq<_>) =
-    let equal = Seq.forall2(fun x y -> x = y)
-    equal a b
+let resolveQ (s:List<(int * int)>) =
+    let last = s |> Seq.last
+    let chop = s |> List.findIndex(fun x -> x = last)
+    s |> Seq.skip chop |> Seq.take (s.Length - chop-1) |> Seq.map(fun (n, d) -> n/d) |> Seq.toList
 
-let getRepGroup (chrs:List<char>) = 
-    let rec windowGroup (group:List<char>) (running:List<List<char>>) = 
-        match group with
-        | list when list |> List.length = 1 -> windowGroup list.Tail running //One character in length. Can't repeat, so skip.
-        | list -> 
-            List.Empty
-        | [] -> running |> List.maxBy List.length
-    windowGroup chrs List.Empty
+let answer = 
+    [1..1000]
+    |> Seq.map(fun x ->(x, (repeater 1 x) |> resolveQ))
+    |> Seq.sortBy (fun (n, x) -> x |> List.length)
+    |> Seq.last
+    |> fst
 
-(* let answer = 
-    [1..999]
-    |> Seq.map(fun x -> (x, x |> divide |> countMaxRep))
-    |> Seq.maxBy snd
-    |> fst *)
-
-//printfn "answer = %i" answer
-
-let six = divide 6
-printfn "%s" (new string(six |> Seq.toArray))
+printfn "%A" answer
--- Problem033/Problem033.fs
+++ Problem033/Problem033.fs
@@ -1,32 +1,27 @@
 ﻿let nums = seq {
-    for n in [10.0..99.0] do
-        for d in [10.0..99.0] do
-            if d > n then
-                yield (n, d)
+    for d in [10.0..99.0] do
+        for n in [10.0..(d-1.0)] do
+            yield (n, d)
 }
 
-let reduce (n, d) =
-    let rec gcd n d =
-        match d with
-        | d when d > 0.0 -> gcd d (n % d)
-        | _ -> n
+let simplify (n, d) =
+    let rec gcd n d = if d = 0.0 then n else gcd d (n % d)
     let gcf = gcd n d
     (n / gcf, d / gcf)
 
-let cancels (n, d) =
-    let ns, ds = n.ToString(), d.ToString()
-    if ns.[0] = ds.[1] && float(int(ns.[1]) - 0x30) / float(int(ds.[0]) - 0x30) = n / d then
-        true
-    else if ns.[1] = ds.[0] && float(int(ns.[0]) - 0x30) / float(int(ds.[1]) - 0x30) = n / d then
-        true
-    else
-        false
+let cancels (n:float, d:float) =
+    let map (s:float) = s.ToString().ToCharArray() |> Array.map(fun x -> float(int(x) - 0x30))
+    let ns, ds = map(n), map(d)
+    match (ns, ds) with
+    | (ns, ds) when ns.[0] = ds.[1] -> ns.[1] / ds.[0] = n / d
+    | (ns, ds) when ns.[1] = ds.[0] -> ns.[0] / ds.[1] = n / d
+    | _ -> false
 
 let answer =
     nums
     |> Seq.filter cancels
     |> Seq.reduce(fun (accn, accd) (n, d) -> (accn*n, accd*d))
-    |> reduce
+    |> simplify
     |> snd
 
 printfn "%A" answer
