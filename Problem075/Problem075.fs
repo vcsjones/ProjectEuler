6ab0b260a97298df34515c615ecd6541c9dc9c97
vcsjones <kevin@vcsjones.com>
2013-07-01 11:44:31 -0400

    Solved problem 75.

---
+++ Problem075/Problem075.fs
@@ -0,0 +1,30 @@
+﻿type Triangle =
+    {
+        a: int64
+        b: int64
+        c: int64
+    }
+    member this.Perimeter =
+            this.a + this.b + this.c
+    static member (+) (left : Triangle, right : Triangle) =
+        {a=left.a + right.a; b=left.b+right.b; c = left.c + right.c}
+            
+
+let rec gcd n d = if d = 0L then n else gcd d (n % d)
+
+let limit = 1500000L
+
+let answer = 
+    [2L..System.Math.Sqrt(float(limit/2L)) |> int64]
+    |> Seq.map(fun m -> ( Seq.init (m |> int) (fun _ -> m), [1L..m]))
+    |> Seq.map(fun (l1, l2) -> Seq.zip l1 l2)
+    |> Seq.concat
+    |> Seq.filter(fun (m, n) -> (n+m)%2L=1L && (gcd n m) = 1L)
+    |> Seq.map(fun (m, n) -> {a = m*m+n*n;b = m*m-n*n;c=2L*m*n;})
+    |> Seq.map(fun t -> t |> Seq.unfold(fun t2 -> Some(t2, t2 + t)) |> Seq.takeWhile(fun t -> t.Perimeter <= limit))
+    |> Seq.concat
+    |> Seq.groupBy(fun t -> t.Perimeter)
+    |> Seq.filter(fun (_, t) -> t |> Seq.length = 1)
+    |> Seq.map(fun (_, t) -> t |> Seq.exactlyOne)
+
+printfn "%A" (answer |> Seq.length)
