17a9d77db91424b53e757451ee5c19e97a70da05
vcsjones <kevin@vcsjones.com>
2013-07-02 14:23:14 -0400

    Refactored to combine the map/concat into a collect.

--- Problem075/Problem075.fs
+++ Problem075/Problem075.fs
@@ -17,12 +17,10 @@ let limit = 1500000L
 let answer = 
     [2L..System.Math.Sqrt(float(limit/2L)) |> int64]
     |> Seq.map(fun m -> ( Seq.init (m |> int) (fun _ -> m), [1L..m]))
-    |> Seq.map(fun (l1, l2) -> Seq.zip l1 l2)
-    |> Seq.concat
+    |> Seq.collect(fun (l1, l2) -> Seq.zip l1 l2)
     |> Seq.filter(fun (m, n) -> (n+m)%2L=1L && (gcd n m) = 1L)
     |> Seq.map(fun (m, n) -> {a = m*m+n*n;b = m*m-n*n;c=2L*m*n;})
-    |> Seq.map(fun t -> t |> Seq.unfold(fun t2 -> Some(t2, t2 + t)) |> Seq.takeWhile(fun t -> t.Perimeter <= limit))
-    |> Seq.concat
+    |> Seq.collect(fun t -> t |> Seq.unfold(fun t2 -> Some(t2, t2 + t)) |> Seq.takeWhile(fun t -> t.Perimeter <= limit))
     |> Seq.groupBy(fun t -> t.Perimeter)
     |> Seq.filter(fun (_, t) -> t |> Seq.length = 1)
     |> Seq.map(fun (_, t) -> t |> Seq.exactlyOne)
