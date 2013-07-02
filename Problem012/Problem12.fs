a2af32dcc0ffeee0edd500fc0aa1c780e0befb83
vcsjones <kevin@vcsjones.com>
2013-07-02 14:21:12 -0400

    Refactored to combine the map/concat into a collect.

--- Problem012/Problem12.fs
+++ Problem012/Problem12.fs
@@ -1,8 +1,7 @@
 ﻿let triangleNumbers = Seq.unfold(fun (p, c) -> Some((p, c), (p+1, c+p))) (2, 1) |> Seq.map(fun (p, c) -> c)
 let countDivisors x = [1..x|>float|>sqrt|>int]
                       |> Seq.filter(fun y -> x%y=0)
-                      |> Seq.map(fun y -> [y;x/y])
-                      |> Seq.concat
+                      |> Seq.collect(fun y -> [y;x/y])
                       |> Seq.length
 
 let answer = 
