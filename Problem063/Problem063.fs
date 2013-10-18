72967582062e112e19a79e015714fa12f73901f2
vcsjones <kevin@vcsjones.com>
2013-10-18 11:59:40 -0400

    Solved problem 63.

--- Problem063/Problem063.fs
+++ Problem063/Problem063.fs
@@ -1,12 +1,11 @@
-﻿let (^*) (x:bigint) (n:bigint) =
-   let mutable result = 1I
-   let mutable xm = x
-   let mutable nm = n
-   while not nm.IsZero do
-      if not nm.IsEven then
-         result <- result * xm
-         nm <- nm-1I
-      xm <- xm**2
-      nm <- nm/2I
-   result
+﻿open System
 
+let rec calculate (n : double) result lowerBound = 
+    match lowerBound with
+    | l when l >= 10 -> result
+    | _ ->
+        let newLowerBound = Math.Pow(10.0, (n-1.0)/n) |> ceil |> int
+        calculate (n+1.0) (result + 10 - newLowerBound) newLowerBound
+    
+let answer = calculate 1.0 0 0
+printfn "%i" answer
